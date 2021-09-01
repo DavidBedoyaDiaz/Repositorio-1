bb7ba5bd1620a461d7142937263ef06daa6d5c3b
David Bedoya Díaz <87878116+DavidBedoyaDiaz@users.noreply.github.com>
2021-09-01 15:02:08 -0500

    Add files via upload

---
+++ Demo_Formulario/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace Demo_Formulario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btncolor = new System.Windows.Forms.Button();
+            this.BtnSaludar = new System.Windows.Forms.Button();
+            this.LblBienvenido = new System.Windows.Forms.Label();
+            this.BtnRojo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btncolor
+            // 
+            this.Btncolor.Location = new System.Drawing.Point(199, 221);
+            this.Btncolor.Name = "Btncolor";
+            this.Btncolor.Size = new System.Drawing.Size(84, 32);
+            this.Btncolor.TabIndex = 0;
+            this.Btncolor.Text = "Azul";
+            this.Btncolor.UseVisualStyleBackColor = true;
+            this.Btncolor.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnSaludar
+            // 
+            this.BtnSaludar.Location = new System.Drawing.Point(470, 221);
+            this.BtnSaludar.Name = "BtnSaludar";
+            this.BtnSaludar.Size = new System.Drawing.Size(75, 32);
+            this.BtnSaludar.TabIndex = 1;
+            this.BtnSaludar.Text = "Saludar";
+            this.BtnSaludar.UseVisualStyleBackColor = true;
+            this.BtnSaludar.Click += new System.EventHandler(this.BtnSaludar_Click);
+            // 
+            // LblBienvenido
+            // 
+            this.LblBienvenido.AutoSize = true;
+            this.LblBienvenido.Font = new System.Drawing.Font("Jokerman", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblBienvenido.Location = new System.Drawing.Point(192, 27);
+            this.LblBienvenido.Name = "LblBienvenido";
+            this.LblBienvenido.Size = new System.Drawing.Size(404, 93);
+            this.LblBienvenido.TabIndex = 2;
+            this.LblBienvenido.Text = "Bienvenido";
+            // 
+            // BtnRojo
+            // 
+            this.BtnRojo.Location = new System.Drawing.Point(199, 306);
+            this.BtnRojo.Name = "BtnRojo";
+            this.BtnRojo.Size = new System.Drawing.Size(84, 32);
+            this.BtnRojo.TabIndex = 3;
+            this.BtnRojo.Text = "Rojo";
+            this.BtnRojo.UseVisualStyleBackColor = true;
+            this.BtnRojo.Click += new System.EventHandler(this.BtnRojo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnRojo);
+            this.Controls.Add(this.LblBienvenido);
+            this.Controls.Add(this.BtnSaludar);
+            this.Controls.Add(this.Btncolor);
+            this.Name = "Form1";
+            this.Text = "Primera aplicación";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btncolor;
+        private System.Windows.Forms.Button BtnSaludar;
+        private System.Windows.Forms.Label LblBienvenido;
+        private System.Windows.Forms.Button BtnRojo;
+    }
+}
+
---
+++ Demo_Formulario/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo_Formulario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Blue;
+        }
+
+        private void BtnRojo_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Red;
+        }
+
+        private void BtnSaludar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bienvenido SAPO PERRO");
+        }
+    }
+}
